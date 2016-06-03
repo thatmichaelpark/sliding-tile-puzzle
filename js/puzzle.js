5d21e98bd46dd883f6072d4d83ddb54c9cb1cef9
Michael Park <thatmichaelpark@gmail.com>
2016-06-03 16:06:12 -0700

    ES6ified

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,116 +1,119 @@
 (function() {
   'use strict';
 
-  var $missing;
-  var missingi;
-  var missingj;
-  var $tiles;
-  var size;
-  var tileWidth;
-  var tileHeight;
-  var moves;
+  let moves;          // Counts player's moves.
 
-  var isSolved = function() {
-    for (var i = 0; i < size; ++i) {
-      for (var j = 0; j < size; ++j) {
-        var match = $tiles[i][j][0].id.match(/i(\d)+j(\d+)/);
-        var k = Number(match[1]);
-        var ll = Number(match[2]);
+  const puzzle = {    // Puzzle object.
+    size: 4,          // Puzzle is size x size tiles.
+    $tiles: null,     // size x size array of $canvas objects.
+    $missing: null,   // The missing piece; points to element of $tile array.
+    tileWidth: 0,     // Width in pixels of a tile on screen.
+    tileHeight: 0,    // Height in pixels of a tile on screen.
+    isSolved() {
+      for (let i = 0; i < this.size; ++i) {
+        for (let j = 0; j < this.size; ++j) {
+          // Turn tile id ('i#j#'') into i0, j0.
+          const match = puzzle.$tiles[i][j][0].id.match(/i(\d)+j(\d+)/);
+          const i0 = Number(match[1]);
+          const j0 = Number(match[2]);
 
-        if (i !== k || j !== ll) {
-          return false;
+          if (i !== i0 || j !== j0) {
+            return false;
+          }
         }
       }
-    }
 
-    return true;
+      return true;
+    }
   };
 
-  var makeClickable = function() {
-    var isInside = function(ii, jj) {
-      return ii >= 0 && ii < size && jj >= 0 && jj < size;
+  const makeClickable = function() {
+  // Makes the tiles around $missing clickable.
+
+    const isInside = function(ii, jj) {
+      return ii >= 0 && ii < puzzle.size && jj >= 0 && jj < puzzle.size;
     };
 
-    var tryToMakeClickable = function(/* ii, jj, top, left, width, height,
-      $div*/) {
-      var ii = arguments[0]; // Hack to avoid eslint too-many-args error.
-      var jj = arguments[1];
-      var top = arguments[2];
-      var left = arguments[3];
-      var width = arguments[4];
-      var height = arguments[5];
-      var $div = arguments[6];
+    const tryToMakeClickable = function(...args) {
+    // args: ii, jj, top, left, width, height, $div
+
+      const ii = args[0]; // Hack to avoid eslint too-many-args error.
+      const jj = args[1];
+      const top = args[2];
+      const left = args[3];
+      const width = args[4];
+      const height = args[5];
+      const $div = args[6];
 
       if (isInside(ii, jj)) {
-        $tiles[ii][jj].addClass('clickable');
+        puzzle.$tiles[ii][jj].addClass('clickable');
         $div.css('top', top);
         $div.css('left', left);
         $div.css('width', width);
         $div.css('height', height);
-        $tiles[ii][jj].draggable({
-          containment: '.' + $div.attr('id') + '}',
+        puzzle.$tiles[ii][jj].draggable({
+          containment: `.${$div.attr('id')}`,
           revert: true
         });
-        $tiles[ii][jj].draggable('enable');
+        puzzle.$tiles[ii][jj].draggable('enable');
       }
     };
 
     $('canvas.clickable').draggable('disable');
     $('canvas').removeClass('clickable');
-    var i = missingi;
-    var j = missingj;
+    const i = puzzle.$missing.i;
+    const j = puzzle.$missing.j;
 
-    tryToMakeClickable(i + 1, j, i * tileHeight, j * tileWidth,
-      tileWidth, tileHeight * 2, $('#channel0'));
-    tryToMakeClickable(i - 1, j, (i - 1) * tileHeight, j * tileWidth,
-      tileWidth, tileHeight * 2, $('#channel1'));
-    tryToMakeClickable(i, j + 1, i * tileHeight, j * tileWidth,
-      tileWidth * 2, tileHeight, $('#channel2'));
-    tryToMakeClickable(i, j - 1, i * tileHeight, (j - 1) * tileWidth,
-      tileWidth * 2, tileHeight, $('#channel3'));
-    $('.droptarget').css('top', missingi * tileHeight);
-    $('.droptarget').css('left', missingj * tileWidth);
-    $('.droptarget').css('width', tileWidth);
-    $('.droptarget').css('height', tileHeight);
+    tryToMakeClickable(i + 1, j,
+      i * puzzle.tileHeight, j * puzzle.tileWidth,
+      puzzle.tileWidth, puzzle.tileHeight * 2, $('#channel0'));
+    tryToMakeClickable(i - 1, j,
+      (i - 1) * puzzle.tileHeight, j * puzzle.tileWidth,
+      puzzle.tileWidth, puzzle.tileHeight * 2, $('#channel1'));
+    tryToMakeClickable(i, j + 1,
+      i * puzzle.tileHeight, j * puzzle.tileWidth,
+      puzzle.tileWidth * 2, puzzle.tileHeight, $('#channel2'));
+    tryToMakeClickable(i, j - 1,
+      i * puzzle.tileHeight, (j - 1) * puzzle.tileWidth,
+      puzzle.tileWidth * 2, puzzle.tileHeight, $('#channel3'));
+    $('.droptarget').css('top', puzzle.$missing.i * puzzle.tileHeight);
+    $('.droptarget').css('left', puzzle.$missing.j * puzzle.tileWidth);
+    $('.droptarget').css('width', puzzle.tileWidth);
+    $('.droptarget').css('height', puzzle.tileHeight);
   };
 
-  $('#puzzleDiv').on('click', 'canvas.clickable', function(event) {
-    moves += 1;
-    var $tile = $(event.target);
-    var i = parseInt($tile.css('top')) / tileHeight;
-    var j = parseInt($tile.css('left')) / tileWidth;
-    var temp = $tile.css('top');
+  $('#puzzleDiv').on('click', 'canvas.clickable', (event) => {
+    const $tile = $(event.target);
+    const i = parseInt($tile.css('top')) / puzzle.tileHeight;
+    const j = parseInt($tile.css('left')) / puzzle.tileWidth;
+    const swapClickedAndMissingTiles = () => {
+      let temp = $tile.css('top');
 
-    (function() { // IIFE hack to avoid eslint fn-too-long error.
-      $tile.css('top', $missing.css('top'));
-      $missing.css('top', temp);
+      $tile.css('top', puzzle.$missing.css('top'));
+      puzzle.$missing.css('top', temp);
       temp = $tile.css('left');
-      $tile.css('left', $missing.css('left'));
-      $missing.css('left', temp);
-      temp = $tiles[i][j];
-      $tiles[i][j] = $tiles[missingi][missingj];
-      $tiles[missingi][missingj] = temp;
-      missingi = i;
-      missingj = j;
-    })();
-    if (isSolved()) {
-      $('#puzzleDiv').prepend($missing);
-      window.Materialize.toast('Solved in ' + moves + ' moves!', 4000);
-      missingi = -1;
-      missingj = -1;
+      $tile.css('left', puzzle.$missing.css('left'));
+      puzzle.$missing.css('left', temp);
+      temp = puzzle.$tiles[i][j];
+      puzzle.$tiles[i][j] = puzzle.$tiles[puzzle.$missing.i][puzzle.$missing.j];
+      puzzle.$tiles[puzzle.$missing.i][puzzle.$missing.j] = temp;
+      puzzle.$missing.i = i;
+      puzzle.$missing.j = j;
+    };
+
+    moves += 1;
+    swapClickedAndMissingTiles();
+    if (puzzle.isSolved()) {
+      $('#puzzleDiv').prepend(puzzle.$missing);
+      window.Materialize.toast(`Solved in ${moves} moves!`, 4000);
+      puzzle.$missing.i = -1;
+      puzzle.$missing.j = -1;
     }
     makeClickable();
   });
 
-  var makePuzzle = function(img, mm) {
-    var width = img.width;
-    var height = img.height;
-    var i;
-    var j;
-    var $row;
-    var $canvas;
-    var ctx;
-    var drawOutline = function(ww, hh) {
+  const makePuzzle = function(img, mm) {
+    const drawOutline = function(ctx, ww, hh) {
       ctx.beginPath();
       ctx.moveTo(0, 0);
       ctx.lineTo(ww, 0);
@@ -120,94 +123,115 @@
       ctx.stroke();
     };
 
-    var createCanvas = function(ii, jj) {
-      $canvas = $('<canvas>');
-      $canvas.ii = ii;
-      $canvas.jj = jj;
-      $canvas.attr('id', 'i' + ii + 'j' + jj);
+    const createCanvas = function(i, j) {
+      const $canvas = $('<canvas>');
+
+      $canvas.i = i;
+      $canvas.j = j;
+      $canvas.attr('id', `i${i}j${j}`);
+
+      return $canvas;
     };
 
-    (function() { // Hack to avoid eslint fn-too-long error.
-      size = mm;
+    const initTiles = () => {
+      puzzle.size = mm;
       moves = 0;
-      $tiles = [];
+      puzzle.$tiles = [];
+      const width = img.width;
+      const height = img.height;
 
-      tileWidth = Math.floor(width / mm);
-      tileHeight = Math.floor(height / mm);
+      puzzle.tileWidth = Math.floor(width / puzzle.size);
+      puzzle.tileHeight = Math.floor(height / puzzle.size);
       $('#puzzleView').css('width', width + 20);
       $('#puzzleView').css('height', height + 20);
       $('#puzzleDiv').empty();
       $('#puzzleDiv').css('width', width);
       $('#puzzleDiv').css('height', height);
 
-      $('#puzzleDiv').append($('<div id="channel0" class="channel0">'));
-      $('#puzzleDiv').append($('<div id="channel1" class="channel1">'));
-      $('#puzzleDiv').append($('<div id="channel2" class="channel2">'));
-      $('#puzzleDiv').append($('<div id="channel3" class="channel3">'));
-      $('#puzzleDiv').append($('<div class="droptarget">'));
-    })();
+      // Stuff for drag&drop (someday):
+      // $('#puzzleDiv').append($('<div id="channel0" class="channel0">'));
+      // $('#puzzleDiv').append($('<div id="channel1" class="channel1">'));
+      // $('#puzzleDiv').append($('<div id="channel2" class="channel2">'));
+      // $('#puzzleDiv').append($('<div id="channel3" class="channel3">'));
+      // $('#puzzleDiv').append($('<div class="droptarget">'));
+    };
 
-    (function() { // Hack to avoid eslint fn-too-long error.
-      for (i = 0; i < mm; ++i) {
-        $row = [];
-        for (j = 0; j < mm; ++j) {
-          createCanvas(i, j);
-          $canvas.attr('width', tileWidth);
-          $canvas.attr('height', tileHeight);
-          ctx = $canvas[0].getContext('2d');
-          ctx.drawImage(img, tileWidth * j, tileHeight * i,
-            tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
-          drawOutline(tileWidth, tileHeight);
+    const renderTiles = () => {
+      for (let i = 0; i < puzzle.size; ++i) {
+        const $row = [];
+
+        for (let j = 0; j < puzzle.size; ++j) {
+          const $canvas = createCanvas(i, j);
+
+          $canvas.attr('width', puzzle.tileWidth);
+          $canvas.attr('height', puzzle.tileHeight);
+
+          const ctx = $canvas[0].getContext('2d');
+
+          ctx.drawImage(img,
+            puzzle.tileWidth * j, puzzle.tileHeight * i,
+            puzzle.tileWidth, puzzle.tileHeight,
+            0, 0,
+            puzzle.tileWidth, puzzle.tileHeight);
+          drawOutline(ctx, puzzle.tileWidth, puzzle.tileHeight);
           $row.push($canvas);
         }
-        $tiles.push($row);
+        puzzle.$tiles.push($row);
       }
-      missingi = Math.floor(Math.random() * mm);
-      missingj = Math.floor(Math.random() * mm);
-      $missing = $tiles[missingi][missingj];
-    })();
+      puzzle.$missing = puzzle.$tiles[Math.floor(Math.random() * puzzle.size)
+        ][Math.floor(Math.random() * puzzle.size)];
+    };
 
-    var swapi;
-    var swapj;
+    const shuffleTiles = () => {
+      let swapi;
+      let swapj;
 
-    (function() { // Hack to avoid eslint fn-too-long error.
-      for (i = 0; i < mm; ++i) {
+      // (Increase # iterations for more thorough randomizing.)
+      for (let i = 0; i < mm; ++i) {
         do {
-          swapi = missingi;
-          swapj = missingj;
+          swapi = puzzle.$missing.i;
+          swapj = puzzle.$missing.j;
           if (Math.random() < 0.5) {
             swapi += Math.random() < 0.5 ? -1 : 1;
           }
           else {
             swapj += Math.random() < 0.5 ? -1 : 1;
           }
-        } while (swapi < 0 || swapi >= mm || swapj < 0 || swapj >= mm);
-        var $temp = $tiles[swapi][swapj];
+        } while (swapi < 0 || swapi >= puzzle.size ||
+                 swapj < 0 || swapj >= puzzle.size);
+        const $temp = puzzle.$tiles[swapi][swapj];
 
-        $missing = $tiles[swapi][swapj] = $tiles[missingi][missingj];
-        $tiles[missingi][missingj] = $temp;
-        missingi = swapi;
-        missingj = swapj;
+        puzzle.$missing = puzzle.$tiles[swapi][swapj] =
+          puzzle.$tiles[puzzle.$missing.i][puzzle.$missing.j];
+        puzzle.$tiles[puzzle.$missing.i][puzzle.$missing.j] = $temp;
+        puzzle.$missing.i = swapi;
+        puzzle.$missing.j = swapj;
       }
-    })();
-    (function() { // Hack to avoid eslint fn-too-long error.
-      for (i = 0; i < mm; ++i) {
-        for (j = 0; j < mm; ++j) {
-          $tiles[i][j].css('top', tileHeight * i);
-          $tiles[i][j].css('left', tileWidth * j);
-          if (i !== missingi || j !== missingj) {
-            $('#puzzleDiv').append($tiles[i][j]);
+    };
+
+    const appendTiles = () => {
+      for (let i = 0; i < mm; ++i) {
+        for (let j = 0; j < mm; ++j) {
+          puzzle.$tiles[i][j].css('top', puzzle.tileHeight * i);
+          puzzle.$tiles[i][j].css('left', puzzle.tileWidth * j);
+          if (i !== puzzle.$missing.i || j !== puzzle.$missing.j) {
+            $('#puzzleDiv').append(puzzle.$tiles[i][j]);
           }
         }
       }
-      makeClickable();
-    })();
+    };
+
+    initTiles();
+    renderTiles();
+    shuffleTiles();
+    appendTiles();
+    makeClickable();
   };
 
-  window.puzzle.create = function(url) {
-    var img = new window.Image();
+  window.createPuzzle = function(url) {
+    const img = new window.Image();
 
-    img.addEventListener('load', function() {
+    img.addEventListener('load', () => {
       makePuzzle(img, 4);
     });
     img.setAttribute('src', url);
